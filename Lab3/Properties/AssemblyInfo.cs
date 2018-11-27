370acb69c18793b0aca7b240029d12c6b83d1ecb
Fiodar <f.motin@sam-solutions.com>
2018-11-28 01:25:57 +0300

    remove local files

--- Lab3/Properties/AssemblyInfo.cs
+++ Lab3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Lab2")]
+[assembly: AssemblyTitle("Lab3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lab2")]
+[assembly: AssemblyProduct("Lab3")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("dbc0c3cb-e373-4f55-ad5f-5e81394d1523")]
+[assembly: Guid("85fb17e3-9328-4620-ab9f-7653d5c880be")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
